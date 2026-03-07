bb4e95e78bc6eaa0cd5c56b1bd92e83c75695fdc
haikalmsg <117971970+haikalmsg@users.noreply.github.com>
2026-03-07 21:43:48 +0700

    added cors handling

--- app/main.py
+++ app/main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
 from app.api.router import api_router
 from app.core.database import Base, engine
+from fastapi.middleware.cors import CORSMiddleware
 
 
+app = FastAPI(title="Personal Finance API", version="1.0.0", description="API for managing personal finance, including accounts, transactions, and categories.")
 
-app = FastAPI(title="Ledger")
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 Base.metadata.create_all(bind=engine)
 app.include_router(api_router)
